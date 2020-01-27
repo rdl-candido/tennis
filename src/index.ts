b50d4fa8b7d9e3b6704634578b59c240a7e7bc44
Rodolfo Candido <rodolfo@5andhalf.com>
2020-01-27 19:31:00 +1100

    Add more scripts to package.json and print return from match score

--- src/index.ts
+++ src/index.ts
@@ -4,22 +4,22 @@ const match = new Match("player 1", "player 2");
 match.pointWonBy("player 1");
 match.pointWonBy("player 2");
 // this will return "0-0, 15-15"
-match.score();
+console.log(match.score());
 
 match.pointWonBy("player 1");
 match.pointWonBy("player 1");
 // this will return "0-0, 40-15"
-match.score();
+console.log(match.score());
 
 match.pointWonBy("player 2");
 match.pointWonBy("player 2");
 // this will return "0-0, Deuce"
-match.score();
+console.log(match.score());
 
 match.pointWonBy("player 1");
 // this will return "0-0, Advantage player 1"
-match.score();
+console.log(match.score());
 
 match.pointWonBy("player 1");
 // this will return "1-0"
-match.score();
+console.log(match.score());
